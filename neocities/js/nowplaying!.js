cde4a8fcaa4ae3b8fbd4e82178b0ebcd9383923e
Selkiefawn <136149232+Selkiefawn@users.noreply.github.com>
2025-03-27 00:40:50 -0400

    Fixed all of the member pages, started the members index, and make some shells that will become something later.

--- neocities/js/nowplaying!.js
+++ neocities/js/nowplaying!.js
@@ -6,7 +6,7 @@ window.onload = function () {
             <svg id="graphicParent" width="350" height="600" viewBox="0 0 1300 2200">
                 <g id="swingGroup">
                     <path id="string" d="M600,-30 Q600,300 600,600" stroke="white" fill="none" stroke-width="18" />
-                    <path id="string2" d="M600, 1160 Q600,1000 600, 1600" stroke="black" fill="none" stroke-width="15" /> 
+                    <path id="string2" d="M600, 1160 Q600,1000 600, 1600" stroke="gray" fill="none" stroke-width="8" /> 
                     <path id="cloud" stroke="#c6d6e7" fill="white" stroke-width="22" stroke-linejoin="round"
                         d="M600,607.4c-14.4-5.4-68.7-24-132.5-1.6-62.3,21.8-93,68.4-101.2,81.9-9.9,2.5-74.4,19.7-108.2,85.2-31.4,60.7-13.4,118.5-10,129-34.4,43.8-37.8,104.3-8.7,150.3,26.9,42.5,76.9,65.5,127.7,58.7,5.2,8.6,41.1,66.4,113.8,78.7,59.4,10,104.4-17,115.4-24,9.9,9.3,59.5,53.8,134.9,52,64-1.5,106.8-35.4,119-45.9,5,.4,80.4,5,127.2-57.1,45.7-60.6,22.8-129.8,20.9-135.2,6.2-7.7,57.6-73.7,36.7-156.9-17.6-69.8-78.9-123.2-154.8-134.1-9-16-40.2-66.9-104.7-92.4-86.8-34.2-164,5.1-175.7,11.3Z" />
                     <path id="star" stroke="#FFF5CF" stroke-width="22" stroke-linejoin="round" fill="white" d="M610,1590l64.3,71.2,87.6,39.2c7.1,3.2,10,11.9,6.1,18.7l-47.9,83.2-10.2,95.4c-.8,7.8-8.2,13.1-15.9,11.5l-93.9-19.8-93.9,19.8c-7.7,1.6-15-3.7-15.9-11.5l-10.2-95.4-47.9-83.2c-3.9-6.8-1.1-15.5,6.1-18.7l87.6-39.2,64.3-71.2c5.2-5.8,14.4-5.8,19.6,0Z"/>
@@ -35,75 +35,57 @@ window.onload = function () {
 
     let currentSong = null;
 
-    songs.muted = true;
-
+    // Initialize Songs
     songs.forEach(song => {
         song.pause();
         song.currentTime = 0;
         song.muted = true;
-        // console.log("Song Muted");
     });
 
     // Select a random song
-    const randomIndex = Math.floor(Math.random() * songs.length);
-    currentSong = songs[randomIndex];
+    currentSong = songs[Math.floor(Math.random() * songs.length)];
 
-    // Get stored volume and pause/play state
+    // Set volume
     const savedVolume = localStorage.getItem('songVolume');
-    currentSong.volume = savedVolume ? savedVolume : currentSong.getAttribute('data-volume');
+    currentSong.volume = savedVolume ?? currentSong.getAttribute('data-volume');
     volumeSlider.value = currentSong.volume;
 
-    const isPaused = localStorage.getItem('isPaused') === 'true';
-    if (isPaused) {
-        pauseButton.textContent = '▶';
-    } else {
-        currentSong.play();
-        currentSong.muted = false;
-        // console.log("Song Unmuted");
-    }
-
-    // Song Info
+    // Set Song Info
     const title = currentSong.getAttribute('data-title');
     const writer = currentSong.getAttribute('data-writer');
     const OSTWriter = writer.includes('OST') ? ` from the ${writer}` : writer;
     songInfo.textContent = `Now Playing: ${title}${writer.includes('OST') ? '' : ' by '}${OSTWriter}`;
 
-    pauseButton.addEventListener('click', () => {
-        if (currentSong.paused) {
-            currentSong.play();
-            currentSong.muted = false;
-            resumeAnimations();
-            pauseButton.textContent = '❚❚';
-        } else {
-            currentSong.pause();
-            smoothStop();
-            pauseButton.textContent = '▶'; 
-        }
-    });
+    // Event Listeners
+    pauseButton.addEventListener('click', togglePlayback);
+    volumeSlider.addEventListener('input', updateVolume);
 
-    volumeSlider.addEventListener('input', () => {
+    function updateVolume() {
         currentSong.volume = volumeSlider.value;
         localStorage.setItem('songVolume', volumeSlider.value);
-    });
+    }
+
 
     // Animation Stuffs
 
     let isHovered = false;
 
     const introAnimation = () => {
-        gsap.set("#swingGroup", {  
-            rotate: 0, 
+        gsap.set("#swingGroup", {
+            rotate: 0,
+            transformOrigin: "50% 0%",
         });
-        gsap.set("#string", {  
+        gsap.set("#string", {
             attr: { d: "M600,0 Q600,300 600,600" },
         });
-        gsap.set("#details", { 
+        gsap.set("#details", {
             x: 20,
             rotate: 0
-         });
+        });
 
         gsap.to("#swingGroup", {
             rotate: -10,
+            transformOrigin: "50% 0%",
             duration: 2,
             ease: "power2.inOut",
         });
@@ -115,7 +97,7 @@ window.onload = function () {
         });
 
         gsap.to("#details", {
-            x: 55, 
+            x: 55,
             rotate: -14,
             duration: 2,
             ease: "power2.inOut",
@@ -132,22 +114,23 @@ window.onload = function () {
         gsap.killTweensOf("#details");
 
         gsap.to("#swingGroup", {
-            rotate: 0, // Stop the rotation
-            duration: 1, // Duration for the smooth stop
-            ease: "power4.out", // Ease out for smooth deceleration
+            rotate: 0,
+            duration: 2,
+            transformOrigin: "50% 0%",
+            ease: "power2.out",
         });
 
         gsap.to("#string", {
-            attr: { d: "M600,0 Q600,300 600,600" }, // Reset string position
-            duration: 1,
-            ease: "power4.out",
+            attr: { d: "M600,0 Q600,300 600,600" },
+            duration: 2,
+            ease: "power2.out",
         });
 
         gsap.to("#details", {
-            x: 20, // Stop the details swing
+            x: 20,
             rotate: 0,
-            duration: 1,
-            ease: "power4.out",
+            duration: 2,
+            ease: "power2.out",
         });
     };
 
@@ -234,8 +217,47 @@ window.onload = function () {
         cloudPlayer.classList.toggle('hidden', !cloudPlayer.classList.contains('visible'));
     });
 
+    // Playback
 
-    if (!currentSong.paused) {
-        resumeAnimations();
+    if (localStorage.getItem('isPaused') === null) {
+        localStorage.setItem('isPaused', 'true');
+    }
+
+    function togglePlayback() {
+        const currentlyPaused = currentSong.paused;
+
+        if (currentlyPaused) {
+            currentSong.play();
+            currentSong.muted = false;
+            pauseButton.textContent = '❚❚';
+            localStorage.setItem('isPaused', 'false');
+            resumeAnimations();
+        } else {
+            currentSong.pause();
+            smoothStop();
+            pauseButton.textContent = '▶';
+            localStorage.setItem('isPaused', 'true');
+        }
+    }
+
+    const isPaused = localStorage.getItem('isPaused') === 'true';
+    updatePlaybackState(isPaused); // Ensure the correct state is set
+
+    function updatePlaybackState(isPaused) {
+        if (isPaused) {
+            currentSong.pause();
+            pauseButton.textContent = '▶';
+        } else {
+            currentSong.play();
+            currentSong.muted = false;
+            pauseButton.textContent = '❚❚';
+            resumeAnimations();
+        }
+    }
+
+    updatePlaybackState(isPaused);
+
+    if (!isPaused) {
+        currentSong.play();
     }
 };
